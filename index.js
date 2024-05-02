a22de4e08e324a4837472c8300da6cdd56f89ead
towhid046 <towhidmorol46@gmail.com>
2024-05-02 14:32:19 +0600

    implement get method to get services data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q1nysvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,7 +32,23 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
 
-    app.get('/')
+    const serviceCollection = client.db("carService").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: {title:1,price:1,  }
+      }
+      const service = await serviceCollection.findOne(query, options);
+      res.send(service);
+    });
 
     // await client.db("admin").command({ ping: 1 });
     console.log(
@@ -40,7 +56,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
